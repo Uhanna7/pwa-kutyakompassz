cf9f14d61b95503505ad70e280d4e8b032feb374
Uri Hanna <h165841@stud.u-szeged.hu>
2023-12-02 16:26:28 +0100

    firebase init

---
+++ kutyakompassz/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+
+import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
+  providers: [ScreenTrackingService, UserTrackingService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ kutyakompassz/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'pwa-kutyakompassz',
+    appId: '1:1038043023102:web:50475bc1c3c82778fe47a',
+    databaseURL:
+      'https://kutyakompassz-a5417-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pwa-kutyakompassz.appspot.com',
+    apiKey: 'AIzaSyD8yaOb9mwKk9SxRet2KMXOjc0khssqHxE',
+    authDomain: 'pwa-kutyakompassz.firebaseapp.com',
+    messagingSenderId: '1038043023102',
+    measurementId: 'G-W8FESB0R8S',
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
